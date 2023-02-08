5757d5ccf4c6e0c4c8e03c047c9b12b9720bb42d
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-02-08 14:00:28 +0700

    adding about and footer

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
     />
+
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
+    Footer,
   },
-  data(){
+  data() {
     return {
-      task:[]
-    }
-  }
-}
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+  },
+};
 </script>
- 
+
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
